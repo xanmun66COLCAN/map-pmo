64b0e08fe31671103fbd9f1fd4f5532942ec1e2c
Alexander Muñoz <xanmun66@Alexanders-MacBook-Pro.local>
2026-07-03 11:15:49 -0400

    feat: login corregido y base de datos sincronizada

--- map-frontend/vite.config.js
+++ map-frontend/vite.config.js
@@ -4,4 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // Redirige las peticiones que empiecen con /api hacia el backend
+      '/api': {
+        target: 'http://localhost:5000', // <-- Cámbialo si tu backend usa otro puerto (ej. 5000)
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
